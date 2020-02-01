f7ddeb41e314e996c7c458a2337fc3e19c646d9c
Marcin <marcin.lozu@gmail.com>
2020-02-01 15:27:44 +0100

    complete

--- todo_app.js
+++ todo_app.js
@@ -1,53 +1,76 @@
-//global variables
+//create variables
 const addBtn = document.getElementById('addBtn');
-const myNodeList = document.getElementsByTagName('li')
-const close = document.getElementsByClassName('close');
 const listElement = document.getElementsByTagName('li');
+const ul = document.getElementById('myUL');
 
-//create "close" button and append it to each list item
-for(let i=0; i < myNodeList.length; i++) {
-	const span = document.createElement('span');
-	const txt = document.createTextNode('\u00D7');
-	span.className = 'close';	
-	span.appendChild(txt);	
-	myNodeList[i].appendChild(span);
-}
+//'close' button
+for(let i=0; i< listElement.length; i++) {
+	let span = document.createElement('span');
+	let txt = document.createTextNode('x');
+	span.className = 'close';
+	span.appendChild(txt);
 
-//Click on a close button to remove element from the list
-const closingFunc = function() {
-	this.parentElement.remove();
+	listElement[i].appendChild(span)
+
 };
 
-for (let i=0;i<close.length; i++) {
-	close[i].onclick = closingFunc;
-	
-};
-//check elements
-const checkedFunction = function(event) {
-	event.target.className = 'checked';
-};
 
-for (let i=0;i < listElement.length; i++) {
-	listElement[i].onclick = checkedFunction;
+const close = document.getElementsByClassName('close');
+//remove elements
+for(let i=0; i< close.length; i++) {
+	close[i].onclick = function() {
+		this.parentElement.remove();
+	}
 };
-
-//create a new list element
+//check and uncheck elements
+for (let i=0; i< listElement.length; i++) {
+	listElement[i].onclick = function(event) {
+		if(this.className === 'checked'){
+			event.target.classList.remove('checked')
+		} else {
+			this.classList.add('checked')
+		}
+	}
+};
+//create elements
 const newElement = function() {
 	let li = document.createElement('li');
 	let inputValue = document.getElementById('myInput').value;
-	let t = document.createTextNode(inputValue);
-	li.appendChild(t);
+	let text = document.createTextNode(inputValue);
+	li.appendChild(text);
 
-
-	if (inputValue === '') {
-		alert('You must write something!')
-	} else {
-		document.getElementById('myUL').appendChild(li)
-	};                                                                       
-
+if (inputValue === '') {
+	alert('You must write sth')
+} else {
+	ul.appendChild(li);
+}
 	document.getElementById('myInput').value = '';
+
+//add close button
+	let span = document.createElement('span');
+	let txt = document.createTextNode('x');
+	span.className = 'close';
+	span.appendChild(txt);
+
+	li.appendChild(span);
+
+//remove element
+for(let i=0; i< close.length; i++) {
+	close[i].onclick = function() {
+		this.parentElement.remove();
+	}
 };
+//check / uncheck
+for (let i=0; i< listElement.length; i++) {
+	listElement[i].onclick = function(event) {
+		if(this.className === 'checked'){
+			event.target.classList.remove('checked')
+		} else {
+			this.classList.add('checked')
+			}
+		}
+	};
 
-//add button action
-addBtn.onclick = newElement;
-
+};
+//
+addBtn.onclick = newElement
