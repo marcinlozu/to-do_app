a195abc20372ccafbc623b51b23171a344259fbb
Marcin <marcin.lozu@gmail.com>
2020-02-01 18:09:44 +0100

    With more function
    Less code, more functions

--- todo_app.js
+++ todo_app.js
@@ -4,34 +4,50 @@ const listElement = document.getElementsByTagName('li');
 const ul = document.getElementById('myUL');
 
 //'close' button
-for(let i=0; i< listElement.length; i++) {
-	let span = document.createElement('span');
-	let txt = document.createTextNode('x');
-	span.className = 'close';
-	span.appendChild(txt);
 
-	listElement[i].appendChild(span)
+const closeFunction = function() {
+	/* body... */
 
+	for(let i=0; i< listElement.length; i++) {
+		let span = document.createElement('span');
+		let txt = document.createTextNode('x');
+		span.className = 'close';
+		span.appendChild(txt);
+
+		listElement[i].appendChild(span)
+
+	};
 };
 
+closeFunction();
 
 const close = document.getElementsByClassName('close');
+
 //remove elements
-for(let i=0; i< close.length; i++) {
-	close[i].onclick = function() {
-		this.parentElement.remove();
-	}
-};
-//check and uncheck elements
-for (let i=0; i< listElement.length; i++) {
-	listElement[i].onclick = function(event) {
-		if(this.className === 'checked'){
-			event.target.classList.remove('checked')
-		} else {
-			this.classList.add('checked')
+const removeFunction = function () {
+	for(let i=0; i< close.length; i++) {
+		close[i].onclick = function() {
+			this.parentElement.remove();
 		}
-	}
+	};
 };
+removeFunction();
+
+//check and uncheck elements
+const checkElementFunction = function () {
+
+	for (let i=0; i< listElement.length; i++) {
+		listElement[i].onclick = function(event) {
+			if(this.className === 'checked'){
+				event.target.classList.remove('checked')
+			} else {
+				this.classList.add('checked')
+			}
+		}
+	};
+};
+checkElementFunction();
+
 //create elements
 const newElement = function() {
 	let li = document.createElement('li');
@@ -39,37 +55,19 @@ const newElement = function() {
 	let text = document.createTextNode(inputValue);
 	li.appendChild(text);
 
-if (inputValue === '') {
-	alert('You must write sth')
-} else {
-	ul.appendChild(li);
-}
+	if (inputValue === '') {
+			alert('You must write sth')
+		} else {
+			ul.appendChild(li);
+		};
+	
 	document.getElementById('myInput').value = '';
 
-//add close button
-	let span = document.createElement('span');
-	let txt = document.createTextNode('x');
-	span.className = 'close';
-	span.appendChild(txt);
+	closeFunction();
 
-	li.appendChild(span);
+	removeFunction();
 
-//remove element
-for(let i=0; i< close.length; i++) {
-	close[i].onclick = function() {
-		this.parentElement.remove();
-	}
-};
-//check / uncheck
-for (let i=0; i< listElement.length; i++) {
-	listElement[i].onclick = function(event) {
-		if(this.className === 'checked'){
-			event.target.classList.remove('checked')
-		} else {
-			this.classList.add('checked')
-			}
-		}
-	};
+	checkElementFunction();
 
 };
 //
